6ccf53f96ecbaa0370f86056d722a8c2a3149d76
Charles <cetyz@hotmail.sg>
2019-09-24 21:34:24 +0800

    Update ScrutineerResultsTabulator.py

--- ScrutineerResultsTabulator.py
+++ ScrutineerResultsTabulator.py
@@ -26,7 +26,7 @@ creds = authenticator.get_creds(SCOPES)
 sheetManager = SheetManager(creds)
 
 df = sheetManager.get_values(sheetId=sheetId, data_range=data_range)
-df.to_csv('raw.csv', encoding='utf_8_sig', index=False)
+#df.to_csv('raw.csv', encoding='utf_8_sig', index=False)
 print(df.columns)
 
 df.loc[:, 'Significance'] = 'No Lift'
@@ -35,49 +35,35 @@ df.loc[df['Min_Abs_Lift_90'] > 0, 'Significance'] = 'Significant Lift'
 df.loc[df['Min_Abs_Lift_95'] > 0, 'Significance'] = 'Strong Significant Lift'
 df.loc[:, 'Abs_Lift'] = df.loc[:,'Conv_Exp'] - df.loc[:, 'Conv_Con']
 
-df = df[['Attribute', 'Cut', 'Question', 'QuestionCategory', 'Answer', 'Conv_Con', 'Conv_Exp', 'Abs_Lift', 'Significance']]
+#df = df[['Attribute', 'Cut', 'Question', 'QuestionCategory', 'Answer', 'Conv_Con', 'Conv_Exp', 'Abs_Lift', 'Significance']]
+#df = df.sort_values(by=['Question', 'Attribute', 'Cut', 'Answer'])
+df = df[['Attribute', 'Cut', 'Question', 'QuestionTitle', 'QuestionCategory', 'Answer', 'AnswerValue', 'Conv_Con', 'Conv_Exp', 'Abs_Lift', 'Significance']]
 df = df.sort_values(by=['Question', 'Attribute', 'Cut', 'Answer'])
 
-df = df.set_index(['Attribute', 'Cut'])
+#df = df.set_index(['Attribute', 'Cut'])
 
-#df.columns = pd.MultiIndex.from_arrays((df['Question'], df['Answer']))
+#print(df)
+df.to_csv('results.csv', encoding='utf_8_sig', index=False)
 
-print(df)
-df.to_csv('results.csv', encoding='utf_8_sig', index=True)
+# let's try to make it pretty with multi level indexing
 
-#values = ['Min_Abs_Lift_80',
-#          'Min_Abs_Lift_90',
-#          'Min_Abs_Lift_95']
+# =============================================================================
+# cols = [] 
+# cols.append(df['Question'].to_list())
+# cols.append(df['Answer'].to_list())
+# 
+# #cols = list(zip(*cols))
+# #cols = pd.MultiIndex.from_tuples(cols, names = ['Question', 'Answer'])
+# 
+# #cols = pd.MultiIndex.from_arrays(arrays = cols, names = ['Question', 'Answer'])
+# cols = pd.MultiIndex.from_frame(df[['Question', 'Answer']])
+# 
+# s = pd.Series(np.random.randn(len(df['Question'].to_list())), index=cols)
+# print(s)
+# 
+# index = []
+# index.append(df['Attribute'].to_list())
+# index.append(df['Cut'].to_list())
+# index = pd.MultiIndex.from_arrays(arrays = index, names = ['Attribute', 'Cut'])
+# =============================================================================
 
-#values = ['Min_Abs_Lift_90',
-#          ]
-#
-#def replace(value, replacement):
-#    if value > 0:
-#        return(replacement)
-#    else:
-#        return('No Lift')
-
-
-
-#table = df.pivot_table(values = values, 
-#                        index = ['Attribute', 'Cut'], 
-#                        columns = ['Question', 'Answer'],
-#                        aggfunc = lambda x: replace(x.item(), 'Significant Lift'))
-
-#table = df.groupby(['Attribute', 'Cut', 'Question', 'Answer']).apply(lambda x: replace(x['Min_Abs_Lift_90'].item(), 'Significant Lift'))
-
-#table = df.groupby(['Attribute', 'Cut', 'Question', 'Answer']).mean()
-#
-#table = table.reset_index()
-#
-#table = df[['Attribute', 'Cut', 'Question', 'Answer', 'Min_Abs_Lift_90', 'Abs_Lift']]
-#
-#table['Significance'] = np.nan
-#
-#table.loc[table['Min_Abs_Lift_90'] > 0, 'Significance'] = 'Significant Lift'
-#table.loc[table['Min_Abs_Lift_90'] <= 0, 'Significance'] = 'No Lift'
-
-#print(table)
-
-#table.to_csv('results.csv', encoding='utf_8_sig')
