ca625b01b68a7d39f00e5e4740d5003963e5ee48
Charles <36413703+cetyz@users.noreply.github.com>
2019-10-03 16:09:24 +0800

    Update ScrutineerResultsTabulator.py

--- ScrutineerResultsTabulator.py
+++ ScrutineerResultsTabulator.py
@@ -8,11 +8,11 @@ Created on Tue Sep 24 16:03:31 2019
 sheetId = '1rlc8Wltr15SCLie4QrbiZjNQXSkD7l19dbBixdhTuCg'
 
 data_range = 'Results'
+lookup_range = 'ScrutineerLookup'
 results_range = 'Results2'
 #sheetId = '1SohobNnQDLN1T95Wl3jMD0QEgV7VgPlX-3pZH43rcE4'
 #data_range ='results-20190913-110925'
 #results_range ='test'
-append = None # 'vertical' or 'horizontal'
 
 from authenticator import Authenticator
 from sheetmanager import SheetManager
@@ -42,6 +42,23 @@ df.loc[:, 'Summary'] = df.loc[:, 'Significance'].str.cat(make_percent(df.loc[:, 
 df.loc[:, 'Summary'] = df.loc[:, 'Summary'].str.cat(make_percent(df.loc[:, 'Conv_Con'], 2), sep='\n')
 df.loc[:, 'Summary'] = df.loc[:, 'Summary'].str.cat(make_percent(df.loc[:, 'Conv_Exp'], 2), sep=' - ')
 
+# create lookup table
+lookup_df = df.copy()
+lookup_df['identifier'] = df['Attribute'].str.cat(df['Cut'], sep='|')
+lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['QuestionTitle'], sep='|')
+lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['Answer'], sep='|')
+lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['AnswerValue'], sep=': ')
+
+lookup_df = lookup_df[['identifier', 'Significance']]
+lookup_values = []
+lookup_values.append(lookup_df.columns.to_list())
+for row in lookup_df.values.tolist():
+    lookup_values.append(row)
+    
+sheetManager.update_values(sheetId=sheetId,
+                           update_range=lookup_range,
+                           values=lookup_values)
+
 # create "header" area
 questions = df.groupby(['Question', 'QuestionTitle', 'Answer'])['AnswerValue'].last()
 
@@ -59,11 +76,16 @@ for question in questions['QuestionTitle'].unique():
     gaps = len(questions.loc[questions['QuestionTitle'] == question, 'Answer'].unique())-1
     questions_row.append(question)
     for gap in range(gaps):
-        questions_row.append('')
+#        questions_row.append('')
+        questions_row.append(question)
     answers = questions.loc[questions['QuestionTitle'] == question, 'Answer'].str.cat(questions.loc[questions['QuestionTitle'] == question, 'AnswerValue'], sep=': ')
     for answer in answers:
         answers_row.append(answer)
 
+
+"""
+=vlookup(concatenate(index(indirect(address(row(), 1, 3, TRUE))),"|",index(indirect(address(row(), 2, 3, TRUE))),"|",index(indirect(address(1,column(),2,TRUE))),"|",index(indirect(address(2,column(),2,TRUE)))), ScrutineerLookup!A:Z, 2, false)
+"""
 values.append(questions_row)
 values.append(answers_row)
 
@@ -79,16 +101,17 @@ for cell in range(len_of_array_formula_row):
 values.append(array_formula_row)
     
 
+formula = '=vlookup(concatenate(index(indirect(address(row(), 1, 3, TRUE))),"|",index(indirect(address(row(), 2, 3, TRUE))),"|",index(indirect(address(1,column(),2,TRUE))),"|",index(indirect(address(2,column(),2,TRUE)))), ScrutineerLookup!A:Z, 2, false)'
+
 # create the attribute and cut columns
 cuts = df.groupby(['Attribute', 'Cut'])['Significance'].last().reset_index()
 for row in cuts[['Attribute', 'Cut']].values.tolist():
     pair = []
     for item in row:
         pair.append(item)
+    for x in range(len_of_array_formula_row):
+        pair.append(formula)
     values.append(pair)
-    
-for row in values:
-    print(row)
 
 
 
@@ -99,6 +122,6 @@ for row in values:
 #        writer.writerow(row)
 
 
-#sheetManager.update_values(sheetId=sheetId,
-#                           update_range=results_range,
-#                           values=values)
+sheetManager.update_values(sheetId=sheetId,
+                           update_range=results_range,
+                           values=values)
