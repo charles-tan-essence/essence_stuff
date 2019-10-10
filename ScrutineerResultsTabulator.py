fd193ab1e1b58df667a598c4caa2b3091fb57e93
Charles <cetyz@hotmail.sg>
2019-10-10 23:57:08 +0800

    Update ScrutineerResultsTabulator.py

--- ScrutineerResultsTabulator.py
+++ ScrutineerResultsTabulator.py
@@ -24,11 +24,12 @@ SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 
 authenticator = Authenticator(keys)
 creds = authenticator.get_creds(SCOPES)
-
 sheetManager = SheetManager(creds)
 
+# get Scrutineer Results
 df = sheetManager.get_values(sheetId=sheetId, data_range=data_range)
 df = df.sort_values(['Question', 'Attribute', 'Cut', 'Answer'])
+# calculate Abs Lift
 df.loc[:, 'Abs_Lift'] = df.loc[:,'Conv_Exp'] - df.loc[:, 'Conv_Con']
 df.loc[:, '%'] = '%'
 
@@ -42,8 +43,6 @@ df.loc[:, 'Summary'] = df.loc[:, 'Significance'].str.cat(make_percent(df.loc[:, 
 df.loc[:, 'Summary'] = df.loc[:, 'Summary'].str.cat(make_percent(df.loc[:, 'Conv_Con'], 2), sep='\n')
 df.loc[:, 'Summary'] = df.loc[:, 'Summary'].str.cat(make_percent(df.loc[:, 'Conv_Exp'], 2), sep=' - ')
 
-#df.to_csv('test.csv')
-
 # create lookup table
 lookup_df = df.copy()
 lookup_df['identifier'] = df['Attribute'].str.cat(df['Cut'], sep='|')
@@ -51,6 +50,7 @@ lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['QuestionTitle'], s
 lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['Answer'], sep='|')
 lookup_df['identifier'] = lookup_df['identifier'].str.cat(df['AnswerValue'], sep=': ')
 
+# select the columns we need and write them to our Lookup Tab
 lookup_df = lookup_df[['identifier', 'Significance', 'Summary', 'Conv_Con', 'Abs_Lift']]
 lookup_values = []
 lookup_values.append(lookup_df.columns.to_list())
@@ -61,12 +61,13 @@ for row in lookup_df.values.tolist():
 #                           update_range=lookup_range,
 #                           values=lookup_values)
 
+# now we start building the list of lists for our magic template tab
+values = []
+
 # create "header" area
 questions = df.groupby(['Question', 'QuestionTitle', 'Answer'])['AnswerValue'].last()
-
 questions = questions.reset_index()
 
-values = []
 buffer_size = 2 # so there is room for the attribute and cuts columns
 questions_row = []
 answers_row = []
@@ -77,19 +78,21 @@ for x in range(buffer_size):
     metric_row.append('')
 
 for question in questions['QuestionTitle'].unique():
+    # get the metric (e.g. Awareness) for each question
     metric = df.loc[df['QuestionTitle'] == question, 'QuestionCategory'].unique()[0]
+    # find out how many times we need to repeat the question (based on the number of answer options)
     gaps = len(questions.loc[questions['QuestionTitle'] == question, 'Answer'].unique())-1
     questions_row.append(question)
     for gap in range(gaps):
-#        questions_row.append('')
         questions_row.append(question)
+    # get the answers and write each one
     answers = questions.loc[questions['QuestionTitle'] == question, 'Answer'].str.cat(questions.loc[questions['QuestionTitle'] == question, 'AnswerValue'], sep=': ')
     for answer in answers:
         answers_row.append(answer)
-#        code_no_lift_row.append(2)
-#        code_lift_row.append(2)
         metric_row.append(metric)
 
+# append each list to our values list
+# header portion complete!
 values.append(questions_row)
 values.append(answers_row)
 values.append(metric_row)
@@ -108,12 +111,21 @@ for cell in range(len_of_array_formula_row):
 summary_col = lookup_df.columns.get_loc('Summary') + 1
 #results_formula = '=vlookup(concatenate(index(indirect(address(row(), 1, 3, TRUE))),"|",index(indirect(address(row(), 2, 3, TRUE))),"|",index(indirect(address(1,column(),2,TRUE))),"|",index(indirect(address(2,column(),2,TRUE)))), ScrutineerLookup!A:Z, '+str(summary_col)+', false)'
 results_formula = '=CONCATENATE(INDIRECT(ADDRESS(ROW()-2,1,1,1)),"|",INDIRECT(ADDRESS(ROW()-2,2,1,1)),"|",INDIRECT(ADDRESS(1,COLUMN(),1,1)),"|",INDIRECT(ADDRESS(2,COLUMN(),1,1)))'
+# for the sample size formula,
+# each attribute/cut pair will have the same sample size throughout all the qns and ans
+# so we just need to get the first one
 sample_size_formula = 'Sample Size Placeholder'
 
 # MAIN BODY
-# create the attribute and cut columns
+# get all the possible attribute/cut combinations
 cuts = df.groupby(['Attribute', 'Cut'])['Significance'].last().reset_index()
-for row in cuts[['Attribute', 'Cut']].values.tolist():
+cuts_list = cuts[['Attribute', 'Cut']].values.tolist()
+
+# get the length of the header row so we know how many times to write the formula
+len_of_header = len(answers_row)
+len_to_write = len_of_header - buffer_size
+
+for row in cuts_list:
     # create a row for each attribute/cut pair
     pair = []
     for item in row:
@@ -121,12 +133,12 @@ for row in cuts[['Attribute', 'Cut']].values.tolist():
     values.append(pair)
     # create a row for the baselines
     baselines = ['', 'Baseline']
-    for x in range(len_of_array_formula_row):
+    for x in range(len_to_write):
         baselines.append('Baseline Placeholder')
     values.append(baselines)
     # create a row for the results, with sample size in front
     results = ['Sample Size:', sample_size_formula]
-    for x in range(len_of_array_formula_row):
+    for x in range(len_to_write):
         results.append(results_formula)
     values.append(results)
 
