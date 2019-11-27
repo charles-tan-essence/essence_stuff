90fded552e4539bec3e4352b64f3e4e43975d853
Charles <36413703+cetyz@users.noreply.github.com>
2019-11-27 17:54:37 +0800

    update

--- Q120 YTP/[New] Match Market Procedure v0 - Standardized.R
+++ Q120 YTP/[New] Match Market Procedure v0 - Standardized.R
@@ -58,101 +58,7 @@ for (lib in libs) {
 #githubinstall("GeoexperimentsResearch")
 #library(GeoexperimentsResearch)
 
-#read in dataset
-ConversionsRaw <- read.csv("signups.csv",header=T)
-ConversionsRaw <- ConversionsRaw[ , c("Day", "Region", "Signups")]
-head(ConversionsRaw)
-
-ConversionsRaw$Day<-as.Date(ConversionsRaw$Day, format="%Y-%m-%d")
-ConversionsData <- ConversionsRaw[complete.cases(ConversionsRaw), ]
-ConversionsData<-ConversionsData[order(ConversionsData$Region),]
-ConversionsData$CountryID <- cumsum(!duplicated(ConversionsData$Region))
-region_names<-ddply(ConversionsData, .(ConversionsData$CountryID, ConversionsData$Region), nrow)
-region_names<-region_names[-3]
-names(region_names)<-c("CountryID","Region")
-region_names
-#write.csv(region_names,"region_names.csv")
-
-
-############################################ 
-#This section will return us the RelativeDistance and Correlation computation 
-#as well as BestControl markets for each region.
 ############################################
-
-###user input
-data_period = c("2018-11-23", "2019-11-23") #select period of data to focus
-###
-
-#output dtw relative distance and correlation scores for all regions
-mm <- best_matches(data = ConversionsData,
-                   id_variable = "CountryID",
-                   date_variable = "Day",
-                   matching_variable = "Signups",
-                   parallel = TRUE,
-                   warping_limit = 1, 
-                   dtw_emphasis = 1, #setting dtw_emphasis to 1 ensures scores are output- no impact to chosen control
-                   matches = length(unique(ConversionsData$Region)), # retrieve scores for all markets for each market
-                   start_match_period = data_period[1],
-                   end_match_period = data_period[2])
-
-head(mm$BestMatches)
-#write.csv(mm$BestMatches, "YTPJPQ319_MatchScores.csv") #to create log of MMT scores
-
-
-############################################ 
-#Applying flags for following constraints to market dataset:
-#- Top 30 percentile lowest dtw distance scores across all geos considered
-#- Correlation of >90%
-#- Average Correlation of Population >50%
-
-#This section will return us the recommended exposed and control markets.
-############################################
-
-###user input
-RD_cutoff = 30 #set cutoff percentile for ave relative distance
-corr_cutoff = 0.5 #set cutoff value for ave correlation of population
-n = 13 #set desired number of exposed markets
-###
-
-length(mm$BestMatches)
-mm$BestMatches['corr_tag_90']<-ifelse(mm$BestMatches$Correlation >=0.9, 1,0)
-mm$BestMatches['corr_tag_80']<-ifelse(mm$BestMatches$Correlation >=0.8, 1,0)
-mm$BestMatches = mutate(mm$BestMatches, percentile_rank = ntile(mm$BestMatches$RelativeDistance,100))
-meandf<-aggregate(mm$BestMatches[,c('Correlation','RelativeDistance')], list(mm$BestMatches$CountryID), mean)
-head(meandf)
-meandf = mutate(meandf, percentile_rank_RD = ntile(meandf$RelativeDistance,100))
-meandf = mutate(meandf, percentile_rank_Corr = ntile(meandf$Correlation,100))
-meandf['pop_ave_dtw_topP']<-ifelse(meandf$percentile_rank_RD < RD_cutoff, 1,0)
-meandf['pop_ave_corr_more']<-ifelse(meandf$Correlation > corr_cutoff, 1,0)
-length(meandf)
-colnames(meandf)[colnames(meandf)=="Group.1"]<-"CountryID"
-newdf <- meandf[,c(1,6,7)]
-MatchScores2 <- merge(mm$BestMatches, newdf, by="CountryID")
-head(MatchScores2)
-colnames(MatchScores2)
-
-#write.csv(MatchScores2,"YTPJPQ120_MatchScores.csv")
-
-MatchScores3<-MatchScores2[MatchScores2$corr_tag_90==1 & MatchScores2$pop_ave_corr_more==1 & MatchScores2$pop_ave_dtw_topP==1,]
-head(MatchScores3)
-nrow(MatchScores3)
-summary(MatchScores3)
-
-ChosenMarkets<-data.frame(table(MatchScores3$CountryID))
-names(ChosenMarkets)[1]<-"CountryID"
-ChosenMarkets<-merge(ChosenMarkets,region_names,by="CountryID")
-head(ChosenMarkets)
-
-exposed <- ChosenMarkets[rev(order(ChosenMarkets$Freq)),]
-exposed2 <- exposed[1:n,1]
-exposed_data <- MatchScores3[MatchScores3$CountryID %in% exposed2 ,]
-exposed_data
-control_mix<-data.frame(table(exposed_data$BestControl))
-names(region_names)[1]<-"Var1"
-control_mix<-merge(control_mix,region_names,by="Var1")
-exposed[1:n,] #list n possible exposed markets with the highest number of potential control pairs
-control_mix[control_mix$Freq==n,] #list control markets common across exposed
-
 ############################################
 #We are going to compare Time-based regression and CausalImpact for back-testing below.
 #User needs to input the pre-test and test dates in test_period and
@@ -165,15 +71,22 @@ control_mix[control_mix$Freq==n,] #list control markets common across exposed
 
 ###user input
 test_period = c("2019-03-01","2019-05-01","2019-05-31")
-geoassign<-data.frame("geo"=c("Tokyo","Saitama Prefecture","Osaka Prefecture",
-                              "Aichi Prefecture","Chiba Prefecture","Ibaraki Prefecture","Kanagawa Prefecture"),
-                      "geo.group"=c(1,1,1,2,2,2,2)) #set exposed and control markets
+geoassign<-data.frame("geo"=c('Osaka Prefecture', 'Kanagawa Prefecture', 'Tokyo', 'Saitama Prefecture', 'Hyogo Prefecture',
+                              'Shizuoka Prefecture', 'Kyoto Prefecture', 'Ibaraki Prefecture', 'Hiroshima Prefecture',
+                              'Miyagi Prefecture', 'Tochigi Prefecture', 'Niigata Prefecture', 'Gunma Prefecture', 'Nagano Prefecture',
+                              'Fukushima Prefecture', 'Gifu Prefecture', 'Okinawa Prefecture', 'Okayama Prefecture', 'Mie Prefecture',
+                              'Kumamoto Prefecture', 'Shiga Prefecture',
+                              'Aichi Prefecture', 'Chiba Prefecture', 'Fukuoka Prefecture', 'Hokkaido Prefecture'),
+                      "geo.group"=c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2)) #set exposed and control markets
 
 ###
 
 #Back-testing with Time-based Regression
-data<-ConversionsRaw
+data<-read.csv('signups.csv', header=T)
 colnames(data)<-c("date","geo","Signups")
+data$date <- as.Date(data$date, format="%Y-%m-%d")
+head(data)
+
 data2<-data[data$date>=test_period[1] & data$date<=test_period[3],]
 head(data2)
 obj.gts<-GeoTimeseries(data2,metrics=c("Signups"))
