d148f5ffdd8b9eb78b233e0d51cffe7a10b39d3e
Charles <36413703+cetyz@users.noreply.github.com>
2020-01-02 18:17:37 +0800

    updates

--- Q419 YT SEA UAC/script.py
+++ Q419 YT SEA UAC/script.py
@@ -44,10 +44,12 @@ test_floodlight_names = [
         ]
 
 base_col_name = 'treatment_user_count'
+control_base_col_name = 'control_user_count'
 treatment_col_name = 'treatment'
 control_col_name = 'scaled_control'
 floodlight_col_name = 'conversion_segment'
 country_col_name = 'country'
+agg_floodlight_col_name = 'Type 276994469 (1248666690 S2S - YouTube Music Premium - Global - Sign Up Complete (from YT Music App))'
         
 
 
@@ -64,12 +66,27 @@ for country in list(raw_data_dict['spreadsheetId'].keys()):
     else:
         df = pd.concat([df, df2], axis=0)
     
-df = df[[country_col_name, floodlight_col_name, base_col_name, control_col_name,
-         treatment_col_name]]
+df = df[[country_col_name, floodlight_col_name, base_col_name, control_base_col_name,
+         control_col_name, treatment_col_name]]
+
+df = df.drop_duplicates(keep='first')
+
+agg_df = df.groupby(country_col_name)[country_col_name,
+                   control_col_name, treatment_col_name].sum().reset_index()
+
+agg_df[floodlight_col_name] = agg_floodlight_col_name
+
+agg_df = agg_df.merge(df[[country_col_name, base_col_name, control_base_col_name]].drop_duplicates(keep='first'),
+                      how='left', on=country_col_name)
+
+df = pd.concat([df, agg_df])
+df = df[[floodlight_col_name, country_col_name, base_col_name,
+         control_base_col_name, treatment_col_name, control_col_name]]
 
 df['control_prop'] = df[control_col_name]/df[base_col_name]
 df['treatment_prop'] = df[treatment_col_name]/df[base_col_name]
 df['abs_lift'] = df['treatment_prop'] - df['control_prop']
+df['rel_lift'] = df['abs_lift'] / df['control_prop']
 df['p-value'] = df.apply(lambda x: ztest(
         [x[treatment_col_name], x[control_col_name]],
         [x[base_col_name], x[base_col_name]]
@@ -80,6 +97,13 @@ df.loc[(df['abs_lift'] > 0) & (df['p-value'] <= 0.2), 'test_significance'] = 'Di
 df.loc[(df['abs_lift'] > 0) & (df['p-value'] <= 0.1), 'test_significance'] = 'Significant Lift'
 df.loc[(df['abs_lift'] > 0) & (df['p-value'] <= 0.05), 'test_significance'] = 'Strong Significant Lift'
 df.loc[(df['abs_lift'] < 0) & (df['p-value'] <= 0.1), 'test_significance'] = 'Negative Lift'
+
+# need to check how to calculate this
+df['incremental_conversions'] = df['abs_lift'] * df[base_col_name]
+
+df['sample_size'] = df[base_col_name] + df[control_base_col_name]
+df['control_sample_prop'] = df[control_base_col_name] / df['sample_size']
+
 
 values = []
 values.append(df.columns.tolist())
